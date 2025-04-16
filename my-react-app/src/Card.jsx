0a60c0f9c9d55ddb3f9a6fbedd19dd918ff08db2
NajeebSultan <najeebsultan070@gmail.com>
2025-04-16 22:48:49 +0500

    Edit app.jsx file

--- my-react-app/src/Card.jsx
+++ my-react-app/src/Card.jsx
@@ -1,8 +1,9 @@
+import profilePic from './assets/profile.jpg'
 
 function Card(){
   return(
     <div className = "card">
-      <img></img>
+      <img  src="https://vita.placeholder.com/150" alt="profile picture"></img>
       <h2>Bro Code</h2>
       <p> I make Youtube videos and play video games</p>
 

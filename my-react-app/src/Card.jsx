6545498a4501c33281182867a2041025dbb28db4
NajeebSultan <najeebsultan070@gmail.com>
2025-04-16 23:09:36 +0500

    Edit profile pic

--- my-react-app/src/Card.jsx
+++ my-react-app/src/Card.jsx
@@ -3,7 +3,7 @@ import profilePic from './assets/profile.jpg'
 function Card(){
   return(
     <div className = "card">
-      <img  src="https://vita.placeholder.com/150" alt="profile picture"></img>
+      <img  src ={profilePic}  alt="profile picture"></img>
       <h2>Bro Code</h2>
       <p> I make Youtube videos and play video games</p>
 

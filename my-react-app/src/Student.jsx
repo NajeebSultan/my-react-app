329348cd6046022a774a6d7571db16180c550e4c
NajeebSultan <najeebsultan070@gmail.com>
2025-04-18 00:06:31 +0500

    Give styling

--- my-react-app/src/Student.jsx
+++ my-react-app/src/Student.jsx
@@ -1,7 +1,7 @@
 
 function Student(props){
   return( 
-    <div>
+    <div className="student">
       <p>Name: {props.name} </p>
       <p>Age: {props.age} </p>
       <p>Student:{props.isStudent ? "yes" : "No"}  </p>

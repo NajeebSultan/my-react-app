6cf254b890c52c62591851168880dc0118656acf
NajeebSultan <najeebsultan070@gmail.com>
2025-04-17 22:47:40 +0500

    update button.module.css file

--- my-react-app/src/Button/Button.jsx
+++ my-react-app/src/Button/Button.jsx
@@ -1,6 +1,6 @@
 import styles from './Button.module.css'
 function Button(){
-  return(<button className="button">Click me</button>
+  return(<button className= {styles.button}>Click me</button>
   );
 }
 export default Button

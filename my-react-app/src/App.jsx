8dc58b40e4d033f6f949a7a863f5070f263fd3b2
NajeebSultan <najeebsultan070@gmail.com>
2025-04-18 21:33:57 +0500

    using boolean value of true and false to display username

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,7 +4,7 @@ import UserGreeting from './UserGreeting.jsx';
 function App() {
   return (
     <>
-<UserGreeting isLoggedIn = {true} username = "BroCode"/>
+<UserGreeting isLoggedIn = {false} username = "BroCode"/>
     </>
   );
 }

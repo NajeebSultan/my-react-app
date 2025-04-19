0d7b912f04d0811db4609fc15be2c8503784796d
NajeebSultan <najeebsultan070@gmail.com>
2025-04-19 14:45:57 +0500

    Make a handelClick funtion wich count click times of button

--- my-react-app/src/Button1.jsx
+++ my-react-app/src/Button1.jsx
@@ -1,11 +1,19 @@
 
 function Button1(){
+  let count = 0;
 
-  const handelClick =() => console.log("OUCH!");
+  const handelClick =(name) => {
+    if(count < 3){
+      count ++;
+      console.log(`${name} you clicked me ${count} time/s `);
 
-  const handelClick2 = (name) => console.log(`${name} stop clicking me`);
+    }
+    else{
+      console.log(`${name} stop clicking me! `);
+    }
+  };
 
-  return(<button onClick={ () => handelClick2("Bro")}>Click me 😊 </button>);
+  return(<button onClick={ () => handelClick ("Bro")}>Click me 😊 </button>);
 
 }
 export default Button1

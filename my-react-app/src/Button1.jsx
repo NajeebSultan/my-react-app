13dccc513704e2803ce702bc306d11c5847d671d
NajeebSultan <najeebsultan070@gmail.com>
2025-04-19 15:01:43 +0500

    Click me button twice will display Ouch message

--- my-react-app/src/Button1.jsx
+++ my-react-app/src/Button1.jsx
@@ -1,19 +1,11 @@
 
 function Button1(){
-  let count = 0;
 
-  const handelClick =(name) => {
-    if(count < 3){
-      count ++;
-      console.log(`${name} you clicked me ${count} time/s `);
 
-    }
-    else{
-      console.log(`${name} stop clicking me! `);
-    }
-  };
+  const handelClick = (e) => e.target.textContent = "OUCH! 😒";
+  
 
-  return(<button onClick={ () => handelClick ("Bro")}>Click me 😊 </button>);
+  return(<button onDoubleClick = {(e) => handelClick (e)}>Click me 😊 </button>);
 
 }
 export default Button1

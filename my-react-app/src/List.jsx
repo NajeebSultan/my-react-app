546222484410c0f7e467a9302dffd1902f8209eb
NajeebSultan <najeebsultan070@gmail.com>
2025-04-19 12:37:55 +0500

    Display only high calories than 100

--- my-react-app/src/List.jsx
+++ my-react-app/src/List.jsx
@@ -11,12 +11,13 @@ function List(){
     //fruits.sort((a,b) => a.calories - b.calories);
     //fruits.sort((a,b) => b.calories - a.calories);
 
-    const lowCalFruit = fruits.filter(fruit => fruit.calories < 100);
+    //const lowCalFruits = fruits.filter(fruit => fruit.calories < 100);
+    //const highCalFruits = fruits.filter(fruit => fruit.calories >= 100);
 
 
-  const listItems = lowCalFruit.map(lowCalFruit => <li key={lowCalFruit.id}>
-    {lowCalFruit.name} : &nbsp;
-    <b>{lowCalFruit.calories}</b></li>);
+  const listItems = highCalFruits.map(highCalFruit => <li key={highCalFruit.id}>
+    {highCalFruit.name} : &nbsp;
+    <b>{highCalFruit.calories}</b></li>);
 
   return(<ol>{listItems}</ol>);
 }

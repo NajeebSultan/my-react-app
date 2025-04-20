5bf2196a3f648327daf445397c391f26014d38d6
NajeebSultan <najeebsultan070@gmail.com>
2025-04-20 15:17:05 +0500

    Make a boolean of isEmployed setter

--- my-react-app/src/MyComponent.jsx
+++ my-react-app/src/MyComponent.jsx
@@ -3,6 +3,7 @@ function MyComponent(){
 
   const [name , setName] = useState("Guest");
   const [age, setAge] = useState(0);
+  const [isEmployed , setIsEmpoyed] = useState(false);
 
   const updateName = () => {
       setName ("Spongebob");
@@ -11,13 +12,19 @@ function MyComponent(){
     setAge(age + 1);
   }
 
+  const toggleEmployedStatus = () => {
+    setIsEmpoyed(!isEmployed);
+  }
   return( <div>
     <p>Name: {name}</p>
-    <button onClick={updateName}>Set Name</button>
-
+    <button onClick={updateName}>Set Name</button> 
 
       <p>Age: {age}</p>
       <button onClick={incrementAge}>Increment Age</button>
+
+      <p>Is employed: {isEmployed ? "yes" : "No"}</p>
+    <button onClick={toggleEmployedStatus}>Toggle Status</button>
+
     </div>
     
 );

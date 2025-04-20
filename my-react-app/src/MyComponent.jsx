8b019c717b7d040cebf75d405a62529c3a30a2df
NajeebSultan <najeebsultan070@gmail.com>
2025-04-20 15:05:06 +0500

    Make a age increment button and setter

--- my-react-app/src/MyComponent.jsx
+++ my-react-app/src/MyComponent.jsx
@@ -2,13 +2,24 @@ import React , {useState} from 'react';
 function MyComponent(){
 
   const [name , setName] = useState("Guest");
+  const [age, setAge] = useState(0);
+
   const updateName = () => {
       setName ("Spongebob");
+  }
+  const incrementAge = () => {
+    setAge(age + 1);
   }
 
   return( <div>
     <p>Name: {name}</p>
     <button onClick={updateName}>Set Name</button>
-  </div>);
+
+
+      <p>Age: {age}</p>
+      <button onClick={incrementAge}>Increment Age</button>
+    </div>
+    
+);
 }
 export default MyComponent

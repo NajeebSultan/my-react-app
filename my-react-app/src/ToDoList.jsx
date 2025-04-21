b8be12ea80c9e209f425d1e96f6b3c256b451f24
NajeebSultan <najeebsultan070@gmail.com>
2025-04-22 00:08:34 +0500

    Make a to-do list

--- my-react-app/src/ToDoList.jsx
+++ my-react-app/src/ToDoList.jsx
@@ -1,5 +1,46 @@
 import React, {useState} from "react";
 function ToDoList(){
-  return(<></>);
+
+
+  const [tasks ,setTasks] = useState(["Eat Breakfast" ,"Take a shower" , "walk the dog"]);
+  const [newTask,setNewTask] = useState("");
+
+  function handleInputChange(event){
+    setNewTask(event.target.value);
+
+  }
+  function addTask(){
+
+  }
+  function deleteTask(index){
+
+  }
+  function moveTaskUp(index){
+    
+  }
+  function moveTaskUp(index){
+    
+  }
+  return( <div className="to-do-list">
+    <h1>To-Do-List</h1>
+    <div>
+      <input type="text" placeholder="Enter a task..." value={newTask} onChange={handleInputChange}  
+   />
+   <button className="add-button" onClick={addTask}></button>
+      </div> 
+<ol>
+  {tasks.map((task,index) =>
+  <li key={index}>
+    <span className="text">{task}</span>
+    <button className="delete-button" onClick={() => deleteTask(index)}>Delete</button>
+
+    <button className="move-button" onClick={() => moveTaskUp(index)}>☝️</button>
+    <button className="move-button" onClick={() =>moveTaskUp(index)}>👇</button>
+    </li>
+
+  )}
+</ol>
+  </div>
+  );
 }
 export default ToDoList
